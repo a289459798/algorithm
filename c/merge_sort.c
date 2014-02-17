366cf305b9fc4eeb985a9cd6773c14ce2c23839b
a289459798 <289459798@qq.com>
2014-02-18 07:44:15 +0800

    去掉最大值

--- c/merge_sort.c
+++ c/merge_sort.c
@@ -32,17 +32,16 @@ void merge(int *arr, int s, int m, int e)
 	int i, j, k;
 	int llen = m-s+1;
 	int rlen = e-m;
-	int larr[llen+1], rarr[rlen+1];
+	int larr[llen], rarr[rlen];
 	for(i = 0; i < llen; i++){
 		larr[i] = arr[s+i];
 	}
 	for(j = 0; j < rlen; j++){
 		rarr[j] = arr[m+j+1];
 	}
-	larr[llen] = rarr[rlen] = 32767;
 	i = j = 0;
 	for(k = s; k <= e; k++){
-		if(larr[i] < rarr[j]){
+		if(j >= rlen || (larr[i] < rarr[j] && i < llen)){
 			arr[k] = larr[i];
 			i++;
 		}else{
